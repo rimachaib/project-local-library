d1402a6f03f4315c889f15f945560b8471795544
Rima Chaib <rima.chaib@gmail.com>
2021-09-02 21:45:06 +0100

    Cleaned up pseudocode in function files.

--- src/accounts.js
+++ src/accounts.js
@@ -1,37 +1,24 @@
-//The following functions can be completed in whichever order I choose.
-
-function findAccountById(accounts, id) {
-let foundId = accounts.find(account => account.id === id);
-return foundId;
-}
 //Create a function named findAccountById which takes in two parameters 1) an array of accounts and 2) an ID of a single account and should return the account object that has the matching ID.
 //Create a variable named foundId that will store the results of the find() method used on the accounts array.
-//Provide an anonymous callback function as the parameter of the find method that will search the accounts array to check if any of the accounts ids match the input argument id. 
+//Provide an anonymous callback function as the parameter of the find method that will search the accounts array to check if any of the accounts ids match the input argument id.
 //Access the key "id" within the object of the accounts array and compare each account id to the id parameter for strict equality.
 //Return foundId which will have stored the account object that was true for the conditional statement.
-
-function sortAccountsByLastName(accounts) {
-accounts.sort((accountA,accountB) => (accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1));
-return accounts;
+function findAccountById(accounts, id) {
+ let foundId = accounts.find((account) => account.id === id);
+ return foundId;
 }
+
 //Create a function named sortAccountsByLastName which takes in an array of account objects.
 //In order to sort throught the accounts array use the sort() method to sort the objects alphabetically.
 //Provide an anonymous callback function as the parameter of the sort() method that will compare the last name of account element, accountA to the last name of account element accountB.
 //If the number returned is negative, the first item (accountA) will be moved before the second item (accountB). The opposite is true if the number is positive. The iteration then continues through the entire array.
 //In order to sort a string use .toLowerCase() so that the strings are compared to each other in the same case format.
 //Return a sorted array of objects alphabetically by last name.
-
-
-function getTotalNumberOfBorrows(account, books) {
-let totalBorrows = 0;
-for (let i = 0; i < books.length; i++){
-  for(let j = 0; j < books[i].borrows.length; j++){
-    if (account.id === books[i].borrows[j].id){
-      totalBorrows += 1;
-    }
-  }
-}
-return totalBorrows;
+function sortAccountsByLastName(accounts) {
+ accounts.sort((accountA, accountB) =>
+  accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1
+ );
+ return accounts;
 }
 
 //This function takes in an account object and an array of all books objects
@@ -39,34 +26,19 @@ return totalBorrows;
 //I need to loop through the books array and then loop through the borrow array in order to check if the borrows id matches the account id; if this is true, I need to add 1 to the counter variable totalBorrows.
 //The two loops will continue until the end of books array.
 //When the loop is complete return the value stored in the totalBorrows variable.
-
-function getBooksPossessedByAccount(account, books, authors) {
-   let result = [];
-   let borrowMatch = [];
-   books.forEach((item) => {
-     const borrowed = item.borrows;
-     const book = {
-       id: item.id,
-       title: item.title,
-       genre: item.genre,
-       authorId: item.authorId,
-       author: {},
-       borrows: {}
-     };
-     const {id, title, genre, authorId, author, borrows} = book;
- 
-     borrowed.forEach((borrow) => {
-       if (borrow.id === account.id && borrow.returned === false) {
-         result.push(book); 
-         borrowMatch.push(borrow);
-         book.borrows = borrowMatch;
-         book.author = authors.filter(auth => auth.id === book.authorId)[0];
-       }
-     });
-   });
-   return result;
+function getTotalNumberOfBorrows(account, books) {
+ let totalBorrows = 0;
+ for (let i = 0; i < books.length; i++) {
+  for (let j = 0; j < books[i].borrows.length; j++) {
+   if (account.id === books[i].borrows[j].id) {
+    totalBorrows += 1;
+   }
+  }
  }
-//Declare a variable that will store the value of the final result in an empty array.   
+ return totalBorrows;
+}
+
+//Declare a variable that will store the value of the final result in an empty array.
 //Declare a variable that will store the value of the matching borrow object.
 //Loop through the books array using the forEach method and loop through the borrows array using forEach
 //Declare a variable for just the borrows object const borrowed = item.borrows.
@@ -74,12 +46,36 @@ function getBooksPossessedByAccount(account, books, authors) {
 //As we loop through the borrowed array check if borrow.id is equal to accountId and borrow.returned == false.
 //If conditional is true push the book object into the result array and the borrows object to borrowsMatch array.
 //Filter through the authors array to return one author object whose id matches.
- 
+function getBooksPossessedByAccount(account, books, authors) {
+ let result = [];
+ let borrowMatch = [];
+ books.forEach((item) => {
+  const borrowed = item.borrows;
+  const book = {
+   id: item.id,
+   title: item.title,
+   genre: item.genre,
+   authorId: item.authorId,
+   author: {},
+   borrows: {}
+  };
+  const { id, title, genre, authorId, author, borrows } = book;
 
+  borrowed.forEach((borrow) => {
+   if (borrow.id === account.id && borrow.returned === false) {
+    result.push(book);
+    borrowMatch.push(borrow);
+    book.borrows = borrowMatch;
+    book.author = authors.filter((auth) => auth.id === book.authorId)[0];
+   }
+  });
+ });
+ return result;
+}
 
 module.exports = {
-  findAccountById,
-  sortAccountsByLastName,
-  getTotalNumberOfBorrows,
-  getBooksPossessedByAccount,
+ findAccountById,
+ sortAccountsByLastName,
+ getTotalNumberOfBorrows,
+ getBooksPossessedByAccount
 };
--- src/books.js
+++ src/books.js
@@ -1,55 +1,58 @@
-
-function findAuthorById(authors, id) {
-  let found = authors.find((author) => author.id === id)
-  return found;
-}
-////Create a function named findAuthorById that takes in two parameters 1)An array of authors and 2)An id of a single author
+//Create a function named findAuthorById that takes in two parameters 1)An array of authors and 2)An id of a single author
 //Use the find() method to loop through authors array and search for the author where author.id === id;
 //Return the author object where author.id === id is true.
-
-function findBookById(books, id) {
-  let foundBooks = books.find((book) => book.id === id)
-  return foundBooks;
+function findAuthorById(authors, id) {
+ let found = authors.find((author) => author.id === id);
+ return found;
 }
+
 //Create a function named findBookById that takes in two parameters 1)An array of books and 2)An id of a single author
 //Use the find() method to loop through books array and search for the book where book.id === id;
 //Return the book object where book.id === id is true.
+function findBookById(books, id) {
+ let foundBooks = books.find((book) => book.id === id);
+ return foundBooks;
+}
 
-
+//This function takes a single parameter, an array of books.
+//Return an array with two arrays inside of it. The spread operator be used at the end to combine arrays by using the spread operator.
+//The first array should contain books that have been returned books.borrow.returned === true.
+//The second array should contain books that have been loaned out and are not yet returned books.borrows.returned === false
 function partitionBooksByBorrowedStatus(books) {
-  let booksReturned = books.filter(book => book.borrows.every(borrow => borrow.returned === true));
-  /*The .filter() method will look through the books array and compile a new array that meets our condition.
+ let booksReturned = books.filter((book) =>
+  book.borrows.every((borrow) => borrow.returned === true)
+ );
+ /*The .filter() method will look through the books array and compile a new array that meets our condition.
   Within the filter method we will use a helper function with the every method that will check if our condition
   is true within the borrow array.  If it is true */
-  let booksBorrowed = books.filter(book => book.borrows.some(borrow => borrow.returned === false));
+ let booksBorrowed = books.filter((book) =>
+  book.borrows.some((borrow) => borrow.returned === false)
+ );
  /*The .filter() method will look through the books array and compile a new array that meets our condition.
   Within the filter method we will use a helper function with the .some() method that will check if our condition
   is true within the borrow array.  If it is true */
-  let finalArray = [[...booksBorrowed], [...booksReturned]];
-  return finalArray;
+ let finalArray = [[...booksBorrowed], [...booksReturned]];
+ return finalArray;
 }
-//This function takes a single parameter, an array of books.
-//Return an array with two arrays inside of it. The spread operator be used at the end to combine arrays by using the spread operator. 
-//The first array should contain books that have been loaned out and are not yet returned books.borrows.returned === false
-//The second array should contain books that have been returned books.borrow.returned === true.
 
-function getBorrowersForBook(book, accounts) {
-  return book.borrows.map(borrow => {
-    let account = accounts.find(account => account.id === borrow.id);
-    return {...borrow, ...account};
-  }).slice(0, 10);
-}
 //Create a function named getBorrowersForBook that takes in two parameters 1) A book object and 2)the accounts array.
 //Use the map() method to loop through the borrows array of the book object.
 //Use the find() method within the map method to loop through the accounts array.
 //Pass in an anonymous function as the callback function that takes in each account and finds the account where account.id === borrow.id
 //Return the spread operator that contains the output values of the map method as borrow and the account variable.
 //Use the slice method on the output array to return only the portion of the array up to index value 10 of the returned array.
-
+function getBorrowersForBook(book, accounts) {
+ return book.borrows
+  .map((borrow) => {
+   let account = accounts.find((account) => account.id === borrow.id);
+   return { ...borrow, ...account };
+  })
+  .slice(0, 10);
+}
 
 module.exports = {
-  findAuthorById,
-  findBookById,
-  partitionBooksByBorrowedStatus,
-  getBorrowersForBook,
+ findAuthorById,
+ findBookById,
+ partitionBooksByBorrowedStatus,
+ getBorrowersForBook
 };
